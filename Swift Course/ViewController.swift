b79e4dcfc1c1efb85b040ba37516525045639b72
Senthil kumar K <nightrider.cbe@gmail.com>
2019-01-01 22:50:49 +0530

    commit

---
+++ Swift Course/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Swift Course
+//
+//  Created by SENTHILKUMAR on 01/01/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //Data Types
+        
+        var variable = 10
+        
+        let constant = 20
+        
+        print(variable)
+        
+        variable = 15
+        print(constant)
+        
+        print(variable)
+    }
+
+
+}
+
